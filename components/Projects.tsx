599cda958a552709f9ddba6181d7d691e8642211
shaunn-holden <shaunm78@me.com>
2026-03-28 05:03:05 -0400

    Fix syntax error in Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -100,6 +100,7 @@ const projects = [
         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
       </svg>
     ),
+  },
   {
     id: 8,
     title: "Gym Task Manager",
